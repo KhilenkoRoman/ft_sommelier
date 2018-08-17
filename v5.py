29f985c70f9dc39f2d74ebf2d3b31a7eb0dc6003
Roman KHILENKO <rkhilenk@e1r3p4.unit.ua>
2018-08-17 15:38:51 +0300

    v5 added adaline

--- v5.py
+++ v5.py
@@ -70,6 +70,103 @@ def perceptron(wine_data, epoch_limit=1000, good_thresh=7, bad_thresh=4, learnin
 		epoch += 1
 	return output_data
 
+def transform(input_matrix):
+	res = list(zip(*input_matrix))
+	return res
+
+def predict(input_list, weights_list):
+	output = []
+	prediction = []
+	step_function = lambda x: -1 if x < 0 else 1
+	for i in range(len(input_list)):
+		output.append(0)
+		for j in range(len(input_list[0])):
+			for j in range(len(input_list[0])):
+				output[i] += input_list[i][j] * weights_list[j]
+	for pr in output:
+		prediction.append(step_function(pr))
+	return prediction
+
+
+def adaline(wine_data, epoch_limit=1000, good_thresh=7, bad_thresh=4, learning_rate=0.0001):
+	try:
+		with open(wine_data, newline='') as csvfile:
+			raw_data = list(csv.reader(csvfile, delimiter=';'))
+	except FileNotFoundError as err:
+		print(err.args)
+		return 0
+	except Exception:
+		print('dich')
+		return 0
+
+	# data prepare
+	# traning on pH(legend[8]) and alcohol(legend[10]) parameters
+	legend = raw_data.pop(0)
+	x_array = []
+	y_array = []
+	index = 0
+	for i in range(len(raw_data)):
+		if int(raw_data[i][11]) > good_thresh:
+			x_array.append([])
+			x_array[index].append(1)
+			x_array[index].append(float(raw_data[i][8]))
+			x_array[index].append(float(raw_data[i][10]))
+			x_array[index].append(float(raw_data[i][3]))
+			y_array.append(1)
+			index += 1
+		elif int(raw_data[i][11]) < bad_thresh:
+			x_array.append([])
+			x_array[index].append(1)
+			x_array[index].append(float(raw_data[i][8]))
+			x_array[index].append(float(raw_data[i][10]))
+			x_array[index].append(float(raw_data[i][3]))
+			y_array.append(-1)
+			index += 1
+
+	# prepare vars and step_function for training
+	weights = [random.random(), random.random(), random.random(), random.random()]
+	epoch = 0
+	output_data = []
+	# output_data shoul look like [(current_epoch, num_errors_at_epoch_end, [array_of_weights]), summ_squared_error]
+
+	# training
+	while epoch < epoch_limit:
+		output = []
+		for i in range(len(x_array)):
+			output.append(0)
+			for j in range(len(x_array[0])):
+				output[i] += x_array[i][j] * weights[j]
+		errors = []
+		for i in range(len(x_array)):
+			errors.append(y_array[i] - output[i])
+		feedback = [0] * len(weights)
+		feedback[0] = sum(errors)
+
+		# updating weights
+		t_x_array = transform(x_array)
+		for i in range(len(t_x_array) - 1):
+			for j in range(len(errors)):
+				feedback[i+1] += t_x_array[i+1][j]*errors[j]
+		for i in range(len(weights)):
+			weights[i] += learning_rate * feedback[i]
+		cost = 0
+		for error in errors:
+			cost += error**2
+		cost = cost / 2.0
+
+		# calculating errors
+		result_errors = 0
+		predictions = predict(x_array, weights[:])
+		for i in range(len(y_array)):
+			if y_array[i] != predictions[i]:
+				result_errors += 1
+
+		output_data.append((epoch, result_errors, weights[:], cost))
+		if result_errors == 0:
+			break
+		epoch += 1
+	return output_data
+
 
 def plot_preformace(performance, wine_data, good_thresh, bad_thresh, epoch=-1, save_plot=False):
 	try:
@@ -166,5 +263,6 @@ def plot_preformace(performance, wine_data, good_thresh, bad_thresh, epoch=-1, s
 
 
 if __name__ == '__main__':
-	performance = perceptron("./resources/winequality-red.csv", epoch_limit=20000, learning_rate=0.0001)
+	# performance = perceptron("./resources/winequality-red.csv", epoch_limit=20000, learning_rate=0.0001)
+	performance = adaline("./resources/winequality-red.csv", epoch_limit=20000, learning_rate=0.0001)
 	plot_preformace(performance, "./resources/winequality-red.csv", good_thresh=7, bad_thresh=4, save_plot=False)
